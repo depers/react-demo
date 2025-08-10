4bb7ea9b66290b582826ff611226a1126d48125c
depers <dev_fengxiao@163.com>
2025-08-10 15:11:54 +0800

    6-11 localStorage封装

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,3 +1,30 @@
+import storage from '@/utils/storage'
+import { Button } from 'antd'
+
 export default function Welcome() {
-  return <div>Welcome</div>
+  const handleStorage = (type: number) => {
+    if (type === 1) {
+      storage.set('age', 28)
+      storage.set('user', { name: 'xiaoming', age: 18 })
+    }
+    if (type === 2) {
+      console.log(storage.get('age'))
+      console.log(storage.get('user'))
+    }
+    if (type === 3) {
+      storage.remove('age')
+    }
+    if (type === 4) {
+      storage.clear()
+    }
+  }
+  return (
+    <div>
+      <div>Welcome</div>
+      <Button onClick={() => handleStorage(1)}>写入值</Button>
+      <Button onClick={() => handleStorage(2)}>读取值</Button>
+      <Button onClick={() => handleStorage(3)}>删除值</Button>
+      <Button onClick={() => handleStorage(4)}>清空值</Button>
+    </div>
+  )
 }
