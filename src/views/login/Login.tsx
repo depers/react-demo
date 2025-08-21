9f7fac38ee152f6973a7ed07685a3997fc74f016
depers <dev_fengxiao@163.com>
2025-08-22 07:46:56 +0800

    7-1 登录静态布局实现

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,48 +1,48 @@
-import request from "@/utils/request";
-import { Button, Form, Input } from "antd";
-import "./index.less";
+import request from '@/utils/request'
+import { Button, Form, Input } from 'antd'
+import './index.less'
 
 export default function Login() {
-  const onFinish = () => {};
+  const onFinish = () => {}
 
-  const onFinishFailed = () => {};
+  const onFinishFailed = () => {}
   return (
-    <div className="login">
-      <div className="login-wrapper">
-        <div className="title">系统登录</div>
+    <div className='login'>
+      <div className='login-wrapper'>
+        <div className='title'>系统登录</div>
         <Form
-          name="basic"
+          name='basic'
           labelCol={{ span: 4 }}
           initialValues={{ remember: true }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
-          autoComplete="off"
+          autoComplete='off'
         >
           <Form.Item
-            label="用户名"
-            labelAlign="right"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+            label='用户名'
+            labelAlign='right'
+            name='username'
+            rules={[{ required: true, message: '请输入你的用户名!' }]}
           >
             <Input />
           </Form.Item>
 
           <Form.Item
-            label="密码"
-            labelAlign="right"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            label='密码'
+            labelAlign='right'
+            name='password'
+            rules={[{ required: true, message: '请输入你的密码!' }]}
           >
             <Input.Password />
           </Form.Item>
 
           <Form.Item>
-            <Button block type="primary" htmlType="submit">
+            <Button block type='primary' htmlType='submit'>
               登录
             </Button>
           </Form.Item>
         </Form>
       </div>
     </div>
-  );
+  )
 }
