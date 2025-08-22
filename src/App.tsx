5602c10700d2aff7e86ce78bd71043d08fb6de40
depers <dev_fengxiao@163.com>
2025-08-22 15:29:50 +0800

    7-3 自定义主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import "./App.css";
+import { ConfigProvider } from "antd";
 
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token，影响范围大
+          colorPrimary: "#ed6c00"
+        },
+      }}
+    >
       <RouterProvider router={router} />
-    </>
-  )
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
