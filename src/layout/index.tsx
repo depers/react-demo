05e2de6dfd6e76f5cde8a0de83cce9bfa85a08e7
depers <dev_fengxiao@163.com>
2025-10-05 17:27:55 +0800

    8-2 2. 水印的使用和实现原理、MutationObserver讲解

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons'
-import { Layout, Menu, theme } from 'antd'
+import { Layout, Menu, theme, Watermark } from 'antd'
 
 const { Header, Content, Footer, Sider } = Layout
 
@@ -15,38 +15,66 @@ const App: React.FC = () => {
     token: { colorBgContainer, borderRadiusLG }
   } = theme.useToken()
 
+  useEffect(() => {
+    const target = document.getElementById('content') as HTMLDialogElement
+    const observer = new MutationObserver((mutationsList, observer) => {
+      console.log(mutationsList, observer)
+      console.log('发生了变化。。。。')
+      observer.disconnect()
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          const span = document.createElement('span')
+          span.innerText = 'hello react'
+          target.appendChild(span)
+          observer.observe(target, config)
+        }
+      }
+    })
+
+    const config = {
+      attributes: true,
+      childList: true,
+      subtree: true
+    }
+
+    observer.observe(target, config)
+  }, [])
+
   return (
-    <Layout>
-      <Sider
-        breakpoint='lg'
-        collapsedWidth='0'
-        onBreakpoint={broken => {
-          console.log(broken)
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type)
-        }}
-      >
-        <div className='demo-logo-vertical' />
-        <Menu theme='dark' mode='inline' defaultSelectedKeys={['4']} items={items} />
-      </Sider>
+    <Watermark content='depers'>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG
-            }}
-          >
-            content
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©{new Date().getFullYear()} Created by Ant UED</Footer>
+        <Sider
+          breakpoint='lg'
+          collapsedWidth='0'
+          onBreakpoint={broken => {
+            console.log(broken)
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type)
+          }}
+        >
+          <div className='demo-logo-vertical' />
+          <Menu theme='dark' mode='inline' defaultSelectedKeys={['4']} items={items} />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG
+              }}
+              id='content'
+            >
+              <span>content</span>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©{new Date().getFullYear()} Created by Ant UED</Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </Watermark>
   )
 }
 
