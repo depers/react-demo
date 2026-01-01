ed1cbaec6b5256394710d9742bf322a20140d73b
depers <dev_fengxiao@163.com>
2026-01-01 22:33:40 +0800

    9-2 Echarts图表开发入门

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,43 @@
-import { Descriptions } from 'antd'
+import { Button, Card, Descriptions } from 'antd'
+import * as echarts from 'echarts'
 import styles from './index.module.less'
+import { useEffect } from 'react'
 
 export default function DashBoard() {
+  useEffect(() => {
+    const lineChartDom = document.getElementById('lineChart')
+    const chartInstance = echarts.init(lineChartDom as HTMLElement)
+    chartInstance.setOption({
+      tooltip: {
+        trigger: 'axis'
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '订单',
+          data: [150, 230, 224, 218, 135, 147, 260, 150, 230, 224, 218, 135],
+          type: 'line'
+        },
+        {
+          name: '流水',
+          data: [250, 330, 324, 318, 235, 247, 360, 250, 330, 324, 318, 235],
+          type: 'line'
+        }
+      ]
+    })
+  }, [])
   const userInfo = {
     userImg: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
     userId: 'depers',
@@ -41,6 +77,22 @@ export default function DashBoard() {
           <div className='title'>开通城市</div>
           <div className={styles.data}>1000座</div>
         </div>
+      </div>
+      <div className={styles.chart}>
+        <Card title='订单和流水走势图' extra={<Button type='primary'>刷新</Button>}>
+          <div id='lineChart' className={styles.itemChart}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='司机分布' extra={<Button type='primary'>刷新</Button>}>
+          <div id='pieChartCity' className={styles.itemPie}></div>
+          <div id='pieChartAge' className={styles.itemPie}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='模型诊断' extra={<Button type='primary'>刷新</Button>}>
+          <div id='radarChart' className={styles.itemChart}></div>
+        </Card>
       </div>
     </div>
   )
